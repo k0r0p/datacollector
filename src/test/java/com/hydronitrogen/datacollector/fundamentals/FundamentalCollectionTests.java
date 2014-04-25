7b61021697de18ce1651cad2f85036323e875963
Hamel Ajay Kothari <hkothari@hydronitrogen.com>
2014-04-24 17:15:26 -0700

    cleaning up tests

--- src/test/java/com/hydronitrogen/datacollector/fundamentals/FundamentalCollectionTests.java
+++ src/test/java/com/hydronitrogen/datacollector/fundamentals/FundamentalCollectionTests.java
@@ -1,15 +1,23 @@
 package com.hydronitrogen.datacollector.fundamentals;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
 
 import org.joda.time.DateTime;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.hydronitrogen.datacollector.caching.SecFileCacheService;
 import com.hydronitrogen.datacollector.importer.Filing;
-import com.hydronitrogen.datacollector.importer.MockSecFileCacheService;
 import com.hydronitrogen.datacollector.importer.SecImportServiceImpl;
 import com.hydronitrogen.datacollector.xbrl.XbrlParser;
 
@@ -26,18 +34,30 @@ public final class FundamentalCollectionTests {
     private static final String TEST_FILENAME = "edgar/data/1084869/0001047469-12-008848.txt";
     private static final Filing TEST_FILING = new Filing(TEST_COMPANY, TEST_FORM, TEST_CIK, TEST_DATE, TEST_FILENAME);
 
-    private static final MockSecFileCacheService secFileCacheService = new MockSecFileCacheService();
-    private static final SecImportServiceImpl secImportService = new SecImportServiceImpl(secFileCacheService);
-    private static final XbrlParser TEST_FILING_XBRL = secImportService.getXbrlForFiling(TEST_FILING);
-    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final String TEST_MOCK_FILE = "src/test/resources/flws-20120701.xml";
 
-    static {
+    private SecFileCacheService secFileCacheService;
+    private SecImportServiceImpl secImportService;
+    private XbrlParser testFilingXbrl;
+
+    private ObjectMapper mapper;
+
+    @Before
+    public void setup() throws FileNotFoundException {
+        mapper = new ObjectMapper();
         mapper.registerModule(new GuavaModule());
+
+        secFileCacheService = mock(SecFileCacheService.class);
+        when(secFileCacheService.getFile(any(Path.class), any(Integer.class))).thenReturn(
+                new FileInputStream(TEST_MOCK_FILE));
+
+        secImportService = new SecImportServiceImpl(secFileCacheService);
+        testFilingXbrl = secImportService.getXbrlForFiling(TEST_FILING);
     }
 
     @Test
     public void testSerializeFundamentalCollection() throws JsonProcessingException {
-        FundamentalCollection collection = FundamentalCollection.fromFilingAndXbrl(TEST_FILING, TEST_FILING_XBRL);
+        FundamentalCollection collection = FundamentalCollection.fromFilingAndXbrl(TEST_FILING, testFilingXbrl);
         String json = mapper.writeValueAsString(collection);
         // TODO: (hkothari) this is lazy use org.skyscreamer or something.
         assertTrue(json.contains("\"earningsPerShareBasic\":0.27"));
--- src/test/java/com/hydronitrogen/datacollector/importer/SecImportServiceTest.java
+++ src/test/java/com/hydronitrogen/datacollector/importer/SecImportServiceTest.java
@@ -2,9 +2,16 @@ package com.hydronitrogen.datacollector.importer;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Set;
 
+import org.apache.commons.net.ftp.FTP;
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
@@ -33,11 +40,20 @@ public final class SecImportServiceTest {
     private static final Context TEST_CONTEXT = new Context(TEST_CONTEXT_ID, new Period(false, TEST_CONTEXT_START,
             TEST_CONTEXT_END));
 
-    private final SecFileCacheService secFileCacheService = new MockSecFileCacheService();
+    private static final Path TEST_COMPANY_ZIP_PATH = Paths.get("edgar/full-index/2012/QTR3/company.zip");
+    private static final String TEST_COMPANY_ZIP_FILE = "src/test/resources/company.idx.zip";
+    private static final Path TEST_XBRL_PATH = Paths.get("edgar/data/1084869/000104746912008848/flws-20120701.xml");
+    private static final String TEST_XBRL_FILE = "src/test/resources/flws-20120701.xml";
+
+    private SecFileCacheService secFileCacheService;
     private SecImportServiceImpl secImportService;
 
     @Before
-    public void setUp() {
+    public void setUp() throws FileNotFoundException {
+        secFileCacheService = mock(SecFileCacheService.class);
+        when(secFileCacheService.getFile(TEST_COMPANY_ZIP_PATH)).thenReturn(new FileInputStream(TEST_COMPANY_ZIP_FILE));
+        when(secFileCacheService.getFile(TEST_XBRL_PATH, FTP.ASCII_FILE_TYPE)).thenReturn(
+                new FileInputStream(TEST_XBRL_FILE));
         secImportService = new SecImportServiceImpl(secFileCacheService);
     }
 
